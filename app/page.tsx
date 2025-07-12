6643f373380451cd95a61e6ee73748cf2d06334c
AhmedAlkhaldi93 <ahmed.el-khaldi@hotmail.com>
2025-07-12 19:38:43 +0200

    a

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,15 @@ import { HomePage } from "@/components/home-page/HomePage";
 import { Meal } from "@/components/meal/Meal";
 import { MEAL_SHARE_API } from "@/data/mealShareApi";
 
-
+type MealType = {
+  id: number;
+  title: string;
+  description: string;
+  location: string;
+  when: string;
+  max_reservations: number;
+  price: number;
+}
 export default async function Home() {
   const dataResponse = await fetch(`${MEAL_SHARE_API.BASE_URL}meals`)
   const data = await dataResponse.json();
@@ -11,6 +19,18 @@ export default async function Home() {
     <div>
       <HomePage/>
       {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+      <div className="grid grid-cols-3 gap-4">
+      {(data as MealType[]).map((meal) => (
+        <Meal key={meal.id}
+          title={meal.title}
+          description={meal.description}
+          location={meal.location}
+          date={meal.when}
+          max_reservations={meal.max_reservations}
+          price={meal.price}
+        />
+      ))}
+      </div>
     </div>
   );
 };
